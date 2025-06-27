7d21ca863e4e24e1bed82c0dd3aa75625f066f58
AT- AMIT <127960825+CYBER-AMIT@users.noreply.github.com>
2025-06-26 23:25:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');  
 dotenv.config();
 const app = express();
 
-const verifyToken = require('./middleware/auth');  
+const verifyToken = require('./auth');  
  
 // ---- Middleware ---- //
 app.use(cors({
