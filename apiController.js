595add37aa4a994b7a2198bb0e1e87a4de0a9aa9
AT- AMIT <127960825+CYBER-AMIT@users.noreply.github.com>
2025-06-27 05:07:49 -0700

    Update apiController.js

--- apiController.js
+++ apiController.js
@@ -1,5 +1,5 @@
 // server/controllers/apiController.js
-const User = require('../User');
+const User = require('../models/User');
 const { Quiz, SocialTask } = require('../models/TaskModel'); 
 const Withdrawal = require('../models/WalletModel');
 const mongoose = require('mongoose');
