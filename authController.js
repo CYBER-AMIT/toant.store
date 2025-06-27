dbd7db87c84b82776a58092ffd684f135eaea01c
AT- AMIT <127960825+CYBER-AMIT@users.noreply.github.com>
2025-06-27 05:15:16 -0700

    Update authController.js

--- authController.js
+++ authController.js
@@ -1,5 +1,5 @@
 // server/controllers/authController.js
-const User = require('../models/User');
+const User = require('../User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
